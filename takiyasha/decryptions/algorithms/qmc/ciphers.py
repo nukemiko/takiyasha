69e4ad74449f22a6d303c11a9547a6592ae19287
nukemiko <north666dakota@gmail.com>
2022-01-10 00:03:16 +0800

    removed all loggers

--- takiyasha/decryptions/algorithms/qmc/ciphers.py
+++ takiyasha/decryptions/algorithms/qmc/ciphers.py
@@ -1,4 +1,3 @@
-import logging
 import struct
 from abc import ABCMeta, abstractmethod
 from typing import Final, Generator, Optional
@@ -7,9 +6,6 @@ from ....exceptions import CipherGenerationError
 from ....typehints import BytesType, BytesType_tuple
 
 BE_Uint32 = struct.Struct('>L')
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
 class Cipher(metaclass=ABCMeta):
@@ -23,13 +19,10 @@ class Cipher(metaclass=ABCMeta):
     def __init__(self, key: Optional[BytesType]):
         """Initialize self. See help(type(self)) for accurate signature."""
         if key is None:
-            logger.debug('no key specified')
             self._key: Optional[bytes] = None
         elif isinstance(key, BytesType_tuple):
-            logger.debug(f'key content (as human-readable): {list(key)}')
             self._key: Optional[bytes] = bytes(key)
         else:
-            logger.error(f"'key' must be byte or bytearray, not {type(key).__name__}")
             raise TypeError(f"'key' must be byte or bytearray, not {type(key).__name__}")
     
     @property
@@ -59,24 +52,18 @@ class TEACipher(Cipher):
                  magic_number: int = 0x9e3779b9
                  ):
         """Initialize self. See help(type(self)) for accurate signature."""
-        logger.info('Initializing TEA Cipher')
         if key is None:
             raise TypeError(f"'key' must be byte or bytearray, not None")
         super().__init__(key)
         
-        logger.debug('block size: 8')
         self._block_size: Final[int] = 8
-        logger.debug(f'delta (from magic_number): {magic_number}')
         self._delta: int = magic_number
         
         if self.key_length != 16:
-            logger.error(f'incorrect key length {self.key_length} (should be 16)')
             raise CipherGenerationError(f'incorrect key length {self.key_length} (should be 16)')
         if rounds & 1:
-            logger.error(f'even number of rounds required (got {rounds})')
             raise CipherGenerationError(f'even number of rounds required (got {rounds})')
         
-        logger.debug(f'rounds: {rounds}')
         self._rounds: int = rounds
     
     @property
@@ -115,7 +102,6 @@ class TEACipher(Cipher):
         rounds: int = self.rounds
         
         ciphersum: int = (delta * (rounds // 2)) & 0xffffffff
-        logger.debug(f'cipher sum (as uint32): {ciphersum}')
         
         for i in range(rounds // 2):
             v1 -= ((v0 << 4) + k2) ^ (v0 + ciphersum) ^ ((v0 >> 5) + k3)
@@ -125,10 +111,7 @@ class TEACipher(Cipher):
             ciphersum -= delta
             ciphersum &= 0xffffffff
         
-        logger.debug(f'cipher sum (after operation, as uint32): {ciphersum}')
-        
         ret = self._put_uint32(v0) + self._put_uint32(v1)
-        logger.debug(f'decrypted data content (as human-readable): {list(ret)}')
         return ret
     
     def encrypt(self, src_data: BytesType, /) -> bytearray:
@@ -139,7 +122,6 @@ class TEACipher(Cipher):
         rounds: int = self.rounds
         
         ciphersum: int = 0 & 0xffffffff
-        logger.debug(f'cipher sum (as uint32): {ciphersum}')
         
         for i in range(rounds // 2):
             ciphersum += delta
@@ -149,10 +131,7 @@ class TEACipher(Cipher):
             v1 += ((v0 << 4) + k2) ^ (v0 + ciphersum) ^ ((v0 >> 5) + k3)
             v1 &= 0xffffffff
         
-        logger.debug(f'cipher sum (after operation, as uint32): {ciphersum}')
-        
         ret = self._put_uint32(v0) + self._put_uint32(v1)
-        logger.debug(f'encrypted data content (as human-readable): {list(ret)}')
         return ret
 
 
@@ -161,7 +140,6 @@ class StaticMapCipher(Cipher):
     
     def __init__(self):
         """Initialize self. See help(type(self)) for accurate signature."""
-        logger.info('Initializing StaticCipher')
         super().__init__(key=None)
         
         self.__static_cipher_box: Final[bytes] = bytes(
@@ -211,21 +189,15 @@ class StaticMapCipher(Cipher):
         """Iterates over the encrypted src_data, and yield the decrypted data byte by byte.
         
         Returns an integer between [0, 255] for each iteration."""
-        logger.debug('start to yield decrypted data')
         for i in range(len(src_data)):
             y = src_data[i] ^ self._get_mask(offset=offset + i)
-            if i < 16:
-                logger.debug(f'yield integer {i}/(only first 16 integers): {y}')
             yield y
-        logger.debug('all decrypted data yielded')
     
     def decrypt(self, src_data: BytesType, /, offset: int = 0) -> bytes:
         """Accept encrypted src_data and return the decrypted data.
         
         This method is equal to `bytes(self.iter_decrypt(src_data, offset=offset))`."""
-        logger.info('Start to decrypt data')
         ret = bytes(self.iter_decrypt(src_data, offset=offset))
-        logger.info('Decrypt finished')
         return ret
 
 
@@ -236,7 +208,6 @@ class MapCipher(Cipher):
     
     def __init__(self, key: BytesType):
         """Initialize self. See help(type(self)) for accurate signature."""
-        logger.info('Initializing MapCipher')
         if key is None:
             raise TypeError(f"'key' must be byte or bytearray, not None")
         super().__init__(key)
@@ -257,21 +228,15 @@ class MapCipher(Cipher):
         """Iterates over the encrypted src_data, and yield the decrypted data byte by byte.
 
         Returns an integer between [0, 255] for each iteration."""
-        logger.debug('start to yield decrypted data')
         for i in range(len(src_data)):
             y = src_data[i] ^ self._get_mask(offset=offset + i)
-            if i < 16:
-                logger.debug(f'yield integer {i}/(only first 16 integers): {y}')
             yield y
-        logger.debug('all decrypted data yielded')
     
     def decrypt(self, src_data: BytesType, /, offset: int = 0) -> bytes:
         """Accept encrypted src_data and return the decrypted data.
 
         This method is equal to `bytes(self.iter_decrypt(src_data, offset=offset))`."""
-        logger.info('Start to decrypt data')
         ret = bytes(self.iter_decrypt(src_data, offset=offset))
-        logger.info('Decrypt finished')
         return ret
 
 
