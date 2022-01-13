1e3250cc45f62dcf7b9c3f45c7b7672d92c2bdf9
nukemiko <north666dakota@gmail.com>
2022-01-13 23:36:17 +0800

    removed @abstractmethod decorator for property 'Decrypter.audio_format'; renamed abstract method 'generate()' to 'new()' in Decrypter; added docstrings for class 'Decrypter'; modified docstrings of class 'Cipher'

--- takiyasha/algorithms/common.py
+++ takiyasha/algorithms/common.py
@@ -1,7 +1,8 @@
 from abc import ABCMeta, abstractmethod
 from typing import IO, Optional
 
-from takiyasha.typehints import BytesType, BytesType_tuple
+from ..typehints import BytesType, BytesType_tuple
+from ..utils import get_file_ext_by_header
 
 
 class Cipher(metaclass=ABCMeta):
@@ -10,7 +11,7 @@ class Cipher(metaclass=ABCMeta):
     You cannot directly call it, because it is designed as a template
     for other ciphers.
     
-    Subclasses that inherit from this class must implement the decrypt() method."""
+    Subclasses that inherit from this class must implement the method decrypt()."""
     
     def __init__(self, key: Optional[BytesType]):
         """Initialize self. See help(type(self)) for accurate signature."""
@@ -40,7 +41,15 @@ class Cipher(metaclass=ABCMeta):
 
 
 class Decrypter(metaclass=ABCMeta):
+    """The base class of the decrypter class hierarchy.
+    
+    You cannot directly call it, because it is designed as a template
+    for other ciphers.
+    
+    Subclasses that inherit from this class must implement the classmethod new()."""
+    
     def __init__(self, **kwargs):
+        """Initialize self. See help(type(self)) for accurate signature."""
         self._buffer: IO[bytes] = kwargs.pop('buffer')
         self._cipher: Cipher = kwargs.pop('cipher')
         self._master_key: bytes = kwargs.pop('master_key')
@@ -51,38 +60,67 @@ class Decrypter(metaclass=ABCMeta):
     
     @property
     def buffer(self) -> IO[bytes]:
+        """File object pointing to the source file."""
         return self._buffer
     
     @property
     def cipher(self) -> Cipher:
+        """Instance of the cipher used by the file."""
         return self._cipher
     
     @property
     def master_key(self) -> bytes:
+        """The key used for encryption of the audio data."""
         return self._master_key
     
     @property
     def audio_start(self) -> int:
+        """The offset of the starting position of the audio data."""
         return self._audio_start
     
     @property
     def audio_length(self) -> int:
+        """The length of the audio data."""
         return self._audio_start
     
-    @abstractmethod
     @property
-    def audio_format(self) -> str:
-        pass
+    def audio_format(self) -> Optional[str]:
+        """The format of the audio data.
+        
+        Returns `None` when the audio format is unrecognized."""
+        offset_orig: int = self.buffer.tell()
+        self.buffer.seek(self.audio_start, 0)
+        
+        decrypted: bytes = self.cipher.decrypt(self.buffer.read(16))
+        
+        self.buffer.seek(offset_orig, 0)
+        
+        return get_file_ext_by_header(decrypted)
     
     def reset_buffer_offset(self) -> int:
+        """Change the stream position of `self.buffer`
+        to the offset of the starting position of the audio data.
+        
+        The offset of the starting position of the audio data
+        is given by `self.audio_start`."""
         return self.buffer.seek(self.audio_start, 0)
     
     def read(self, size: int = -1, /) -> bytes:
+        """Read up to size bytes from `self.buffer`
+        and return size bytes of decrypted data.
+        
+        As a convenience, if size is unspecified or -1,
+        all decrypted data until EOF are returned.
+        
+        `self.reset_buffer_offset()` should be called before calling this method,
+        otherwise incorrect data may be returned."""
         offset: int = self.buffer.tell()
         data: bytes = self.buffer.read(size)
+        
         return self.cipher.decrypt(data, offset=offset)
     
     @classmethod
     @abstractmethod
-    def generate(cls, file: IO[bytes]):
+    def new(cls, file: IO[bytes]):
+        """Create a new Decrypter."""
         pass
