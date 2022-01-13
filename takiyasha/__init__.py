a3e4de35e692fbac2f2ddb2c374789a01e992882
nukemiko <north666dakota@gmail.com>
2022-01-13 19:04:59 +0800

    added supported encryption formats to check file type

--- takiyasha/__init__.py
+++ takiyasha/__init__.py
@@ -1,1 +1,8 @@
 VERSION: str = '0.2.0'
+SUPPORTED_FORMATS_PATTERNS = [
+    '*.ncm',
+    '*.qmc[023468]', '*.qmcflac', '*.qmcogg',
+    '*.tkm',
+    '*.mflac', '*.mflac[0]', '*.mgg', '*.mgg[01l]',
+    '*.bkcmp3', '*.bkcm4a', '*.bkcflac', '*.bkcwav', '*.bkcape', '*.bkcogg', '*.bkcwma'
+]
