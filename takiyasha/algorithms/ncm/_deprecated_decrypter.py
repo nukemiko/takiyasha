c7ac5c7acb8bd1520124aa9643bf18a7d4e25c64
nukemiko <north666dakota@gmail.com>
2022-01-14 00:26:15 +0800

    adopt the new NCMDecrypter inherited from the Decrypter base class; deprecated old NCMDecrypter

---
+++ takiyasha/algorithms/ncm/_deprecated_decrypter.py
@@ -0,0 +1,188 @@
+import json
+import struct
+import warnings
+from base64 import b64decode
+from timeit import timeit
+from typing import IO, Optional, Union
+
+from Cryptodome.Cipher import AES
+from Cryptodome.Util.Padding import unpad
+
+from .ciphers import CacheCipher, RC4Cipher
+from ...exceptions import DecryptFailed
+from ...utils import get_file_ext_by_header
+
+LE_Uint32: struct.Struct = struct.Struct('<I')
+
+warnings.warn(DeprecationWarning('this module is deprecated and will be removed in the near future.'))
+
+
+class NCMDecrypter:
+    def __init__(self,
+                 file: IO[bytes],
+                 *,
+                 cipher: Union[RC4Cipher, CacheCipher],
+                 audio_start: int,
+                 master_key: bytes = None,
+                 metadata: dict = None,
+                 metadata_identifier: str = None,
+                 cover_data: bytes = None
+                 ):
+        # check whether file is readable and seekable
+        try:
+            if not (file.readable() and file.read):
+                raise OSError('file is not readable')
+            if not (file.seekable() and file.seek):
+                raise OSError('file is not seekable')
+        except AttributeError:
+            raise TypeError(f"'file' must be readable and seekable file object, not {type(file).__name__}")
+        
+        file.seek(0, 0)  # ensure offset is 0
+        
+        self._buffer: IO[bytes] = file
+        self._cipher: Union[RC4Cipher, CacheCipher] = cipher
+        self._audio_start: int = audio_start
+        self._master_key: bytes = master_key
+        self._metadata: dict = metadata
+        self._metadata_identifier: str = metadata_identifier
+        self._cover_data: bytes = cover_data
+    
+    @property
+    def buffer(self):
+        return self._buffer
+    
+    @property
+    def cipher(self):
+        return self._cipher
+    
+    @property
+    def audio_start(self):
+        return self._audio_start
+    
+    @property
+    def audio_format(self) -> str:
+        """Return the format of decrypted audio data.
+
+        :raise DecryptFailed: failed to recongize audio format.
+        :return: audio format string"""
+        self.buffer.seek(self.audio_start, 0)
+        test_data: bytes = self.buffer.read(16)
+        
+        decrypted_data: bytes = self.cipher.decrypt(test_data)
+        fmt = get_file_ext_by_header(decrypted_data)
+        
+        if not fmt:
+            raise DecryptFailed('failed to recongize decrypted audio format')
+        else:
+            return fmt
+    
+    @property
+    def master_key(self):
+        return self._master_key
+    
+    @property
+    def metadata(self):
+        return self._metadata
+    
+    @property
+    def metadata_identifier(self):
+        return self._metadata_identifier
+    
+    @property
+    def cover_data(self):
+        return self._cover_data
+    
+    def speedtest(self, test_size: int = 1048576):
+        """Calculate the time required to decrypt data of the `test_size` specified size.
+        
+        :return: Calculated result, the unit is second"""
+        self.buffer.seek(self.audio_start, 0)
+        first_data: bytes = self.buffer.read(test_size)
+        
+        stmt = """self.cipher.decrypt(first_data)"""
+        return timeit(stmt, globals=locals(), number=1)
+    
+    def decrypt(self, write_to: IO[bytes] = None) -> Union[bytes, IO[bytes]]:
+        """Decrypt the data from self.buffer, and return the decrypted data.
+        
+        If parameter `write_to` is specified, the decrypted data
+        will be written to the file object pointed to `write_to`.
+        Then, the file object will be returned.
+        
+        :return: Decrypted data or file object"""
+        self.buffer.seek(self.audio_start, 0)
+        data = self.cipher.decrypt(self.buffer.read())
+        if write_to:
+            write_to.write(data)
+            return write_to
+        else:
+            return data
+    
+    @classmethod
+    def generate(cls, file: IO[bytes]):
+        # check whether file is readable and seekable
+        try:
+            if not (file.readable() and file.read):
+                raise OSError('file is not readable')
+            if not (file.seekable() and file.seek):
+                raise OSError('file is not seekable')
+        except AttributeError:
+            raise TypeError(f"'file' must be readable and seekable file object, not {type(file).__name__}")
+        
+        file.seek(0, 0)  # ensure offset is 0
+        
+        # judge file type by magic header
+        if file.read(8) == b'CTENFDAM':
+            # is NCM encrypted file
+            file.seek(2, 1)
+            
+            # read and decrypt master key
+            raw_master_key_len: int = LE_Uint32.unpack(file.read(4))[0]
+            raw_master_key_data: bytes = bytes(b ^ 100 for b in file.read(raw_master_key_len))
+            aes_crypter = AES.new(b'hzHRAmso5kInbaxW', AES.MODE_ECB)
+            master_key_data: bytes = unpad(aes_crypter.decrypt(raw_master_key_data), 16)[17:]
+            master_key: Optional[bytes] = master_key_data
+            
+            # generate RC4Cipher use master key
+            cipher: Union[RC4Cipher, CacheCipher] = RC4Cipher(master_key_data)
+            
+            # read and decrypt metadata
+            raw_metadada_len: int = LE_Uint32.unpack(file.read(4))[0]
+            if raw_metadada_len:
+                raw_metadata: bytes = bytes(b ^ 99 for b in file.read(raw_metadada_len))
+                metadata_identifier: str = raw_metadata.decode()
+                encrypted_metadata: bytes = b64decode(raw_metadata[22:])
+                
+                aes_crypter = AES.new(b"#14ljk_!\\]&0U<'(", AES.MODE_ECB)
+                metadata: dict = json.loads(unpad(aes_crypter.decrypt(encrypted_metadata), 16)[6:])
+            else:
+                metadata_identifier: str = ''
+                metadata: dict = {}
+            
+            file.seek(5, 1)
+            
+            # read cover data
+            cover_space: int = LE_Uint32.unpack(file.read(4))[0]
+            cover_size: int = LE_Uint32.unpack(file.read(4))[0]
+            cover_data: Optional[bytes] = file.read(cover_size) if cover_size else None
+            file.seek(cover_space - cover_size, 1)
+            
+            audio_start: int = file.seek(0, 1)
+        else:
+            # is encrypted Cloudmusic cache file
+            cipher: Union[RC4Cipher, CacheCipher] = CacheCipher()
+            audio_start: int = 0
+            metadata: dict = {}
+            metadata_identifier: str = ''
+            cover_data: Optional[bytes] = None
+            master_key: Optional[bytes] = None
+        
+        return cls(
+            file,
+            cipher=cipher,
+            audio_start=audio_start,
+            master_key=master_key,
+            metadata=metadata.copy(),
+            metadata_identifier=metadata_identifier,
+            cover_data=cover_data
+        )
