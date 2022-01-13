dd6fca7c8f9110c375f24e71b44ca842e751ee56
nukemiko <north666dakota@gmail.com>
2022-01-13 08:01:59 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open('./README.md') as f:
+    readme = f.read()
+
+with open('./requirements.txt') as f:
+    dependencies = f.read()
+dependencies = dependencies.strip().splitlines()
+
+setup(
+    name='takiyasha',
+    version='0.1.0',
+    packages=['takiyasha', 'takiyasha.app', 'takiyasha.algorithms', 'takiyasha.algorithms.ncm', 'takiyasha.algorithms.qmc'],
+    url='https://github.com/nukemiko/takiyasha',
+    license='MIT',
+    author='nukemiko',
+    author_email='',
+    description='DRM protected music unlocker',
+    long_description=readme,
+    python_requires='>=3.8',
+    install_requires=dependencies
+)
