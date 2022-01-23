d07b7d00fc17d627ebab32bc957ef0b4c60f0099
nukemiko <north666dakota@gmail.com>
2022-01-23 22:01:02 +0800

    使用现有代码重写了 'cipher' 模块的 RC4 和 XOR 密码，以及 '__init__' 模块的 'NCMDecrypter'

--- takiyasha/algorithms/ncm/__init__.py
+++ takiyasha/algorithms/ncm/__init__.py
@@ -7,35 +7,48 @@ from typing import IO, Optional, Union
 from Cryptodome.Cipher import AES
 from Cryptodome.Util.Padding import unpad
 
-from .ciphers import NCM_RC4Cipher, NCM_XOROnlyCipher
-from ..common import Decrypter
+from .ciphers import (
+    NCM_NewRC4Cipher,
+    NCM_NewXORCipher,
+    NCM_RC4Cipher,
+    NCM_XOROnlyCipher
+)
+from ..common import (
+    CipherTypeVar,
+    Decoder,
+    Decrypter
+)
+from ...exceptions import DecryptionError
+from ...utils import (
+    get_file_ext_by_header, get_file_name_from_fileobj
+)
 
 LE_Uint32: struct.Struct = struct.Struct('<I')
 
 
 class NCMDecrypter(Decrypter):
     """The decrypter of Cloudmusic NCM encrypted container format."""
-    
+
     def __init__(self, **kwargs):
         """Initialize self. See help(type(self)) for accurate signature."""
         super().__init__(**kwargs)
-        
+
         self._metadata: dict[str, Union[str, list]] = kwargs.pop('metadata')
         self._identifier: str = kwargs.pop('identifier')
         self._cover_data: Optional[bytes] = kwargs.pop('cover_data')
-    
+
     @property
     def metadata(self) -> dict[str, Union[str, list]]:
         return dp(self._metadata)
-    
+
     @property
     def identifier(self) -> str:
         return self._identifier
-    
+
     @property
     def cover_data(self) -> Optional[bytes]:
         return self._cover_data
-    
+
     @classmethod
     def new(cls, file: IO[bytes]):
         # check whether file is readable and seekable
@@ -46,45 +59,45 @@ class NCMDecrypter(Decrypter):
                 raise OSError('file is not seekable')
         except AttributeError:
             raise TypeError(f"'file' must be readable and seekable file object, not {type(file).__name__}")
-        
+
         file.seek(0, 0)  # ensure offset is 0
-        
+
         # judge file type by magic header
         if file.read(8) == b'CTENFDAM':
             # is NCM encrypted file
             file.seek(2, 1)
-            
+
             # read and decrypt master key
             raw_master_key_len: int = LE_Uint32.unpack(file.read(4))[0]
             raw_master_key_data: bytes = bytes(b ^ 100 for b in file.read(raw_master_key_len))
             aes_crypter = AES.new(b'hzHRAmso5kInbaxW', AES.MODE_ECB)
             master_key_data: bytes = unpad(aes_crypter.decrypt(raw_master_key_data), 16)[17:]
             master_key: Optional[bytes] = master_key_data
-            
+
             # generate RC4Cipher use master key
             cipher: Union[NCM_RC4Cipher, NCM_XOROnlyCipher] = NCM_RC4Cipher(master_key_data)
-            
+
             # read and decrypt metadata
             raw_metadada_len: int = LE_Uint32.unpack(file.read(4))[0]
             if raw_metadada_len:
                 raw_metadata: bytes = bytes(b ^ 99 for b in file.read(raw_metadada_len))
                 identifier: str = raw_metadata.decode()
                 encrypted_metadata: bytes = b64decode(raw_metadata[22:])
-                
+
                 aes_crypter = AES.new(b"#14ljk_!\\]&0U<'(", AES.MODE_ECB)
                 metadata: dict = json.loads(unpad(aes_crypter.decrypt(encrypted_metadata), 16)[6:])
             else:
                 identifier: str = ''
                 metadata: dict = {}
-            
+
             file.seek(5, 1)
-            
+
             # read cover data
             cover_space: int = LE_Uint32.unpack(file.read(4))[0]
             cover_size: int = LE_Uint32.unpack(file.read(4))[0]
             cover_data: Optional[bytes] = file.read(cover_size) if cover_size else None
             file.seek(cover_space - cover_size, 1)
-            
+
             audio_start: int = file.tell()
         else:
             # is encrypted Cloudmusic cache file
@@ -94,9 +107,9 @@ class NCMDecrypter(Decrypter):
             identifier: str = ''
             cover_data: Optional[bytes] = None
             master_key: Optional[bytes] = None
-        
+
         audio_length: int = file.seek(0, 2) - audio_start
-        
+
         return cls(
             buffer=file,
             cipher=cipher,
@@ -107,3 +120,113 @@ class NCMDecrypter(Decrypter):
             identifier=identifier,
             cover_data=cover_data
         )
+
+
+class NCMFormatDecoder(Decoder):
+    @classmethod
+    def _pre_create_instance(cls, file: IO[bytes]) -> tuple[bytes, CipherTypeVar, dict[str, ...]]:
+        file.seek(0, 0)
+
+        # 根据文件前8个字节判断文件类型
+        if file.read(8) == b'CTENFDAM':
+            # 文件是 NCM 加密容器格式
+            file.seek(2, 1)
+
+            # 读取和解密主密钥
+            raw_master_key_len: int = LE_Uint32.unpack(file.read(4))[0]
+            raw_master_key_data: bytes = bytes(b ^ 100 for b in file.read(raw_master_key_len))
+            aes_crypter = AES.new(b'hzHRAmso5kInbaxW', AES.MODE_ECB)
+            master_key: bytes = unpad(aes_crypter.decrypt(raw_master_key_data), 16)[17:]
+
+            # 使用 master_key 创建 RC4Cipher
+            cipher: CipherTypeVar = NCM_NewRC4Cipher(master_key)
+
+            # 读取和解密元数据
+            raw_metadada_len: int = LE_Uint32.unpack(file.read(4))[0]
+            if raw_metadada_len:
+                raw_metadata: bytes = bytes(b ^ 99 for b in file.read(raw_metadada_len))
+                identifier: str = raw_metadata.decode()
+                encrypted_metadata: bytes = b64decode(raw_metadata[22:])
+
+                aes_crypter = AES.new(b"#14ljk_!\\]&0U<'(", AES.MODE_ECB)
+                metadata: dict[str, Union[str, list[Union[str, list[str]]], bytes]] = json.loads(unpad(aes_crypter.decrypt(encrypted_metadata), 16)[6:])
+            else:
+                identifier: str = ''
+                metadata: dict[str, Union[str, list[Union[str, list[str]]], bytes]] = {}
+
+            file.seek(5, 1)
+
+            # 读取封面数据
+            cover_space: int = LE_Uint32.unpack(file.read(4))[0]
+            cover_size: int = LE_Uint32.unpack(file.read(4))[0]
+            cover_data: Optional[bytes] = file.read(cover_size) if cover_size else None
+            file.seek(cover_space - cover_size, 1)
+
+            # 将封面数据和网易云音乐独有标识加入元数据
+            metadata['cover_data'] = cover_data
+            metadata['identifier'] = identifier
+
+            # 读取剩余的加密音频数据
+            raw_audio_data: bytes = file.read()
+        else:
+            # 文件或许是网易云音乐的加密缓存
+            file.seek(0, 0)
+
+            cipher: CipherTypeVar = NCM_NewXORCipher()
+            metadata: dict[str, Union[str, list[Union[str, list[str]]], bytes]] = {}
+
+            # 验证文件是否被网易云音乐加密
+            header_data: bytes = file.read(32)
+            if not get_file_ext_by_header(header_data):
+                raise DecryptionError(
+                    f"file '{get_file_name_from_fileobj(file)}' "
+                    f"is not encrypted by Cloudmusic"
+                )
+
+            raw_audio_data: bytes = header_data + file.read()
+
+        return raw_audio_data, cipher, {'metadata': metadata}
+
+    def __init__(
+            self,
+            raw_audio_data: bytes,
+            cipher: CipherTypeVar,
+            misc: dict[str, ...],
+            filename: str
+    ):
+        super().__init__(raw_audio_data, cipher, misc, filename)
+
+    @property
+    def _metadata(self) -> dict[str, Union[str, list[Union[str, list[str]]], bytes]]:
+        return self._misc['metadata']
+
+    @property
+    def music_title(self) -> Optional[str]:
+        return self._metadata.get('musicName')
+
+    @property
+    def music_id(self) -> Optional[int]:
+        if self._metadata.get('musicId'):
+            return int(self._metadata.get('musicId'))
+
+    @property
+    def music_artists(self) -> Optional[list[str]]:
+        if self._metadata.get('artist'):
+            return [item[0] for item in self._metadata.get('artist')]
+
+    @property
+    def music_album(self) -> Optional[str]:
+        return self._metadata.get('album')
+
+    @property
+    def music_album_id(self) -> Optional[int]:
+        if self._metadata.get('albumId'):
+            return int(self._metadata.get('albumId'))
+
+    @property
+    def music_platform_alias(self) -> Optional[list[str]]:
+        return self._metadata.get('alias')
+
+    @property
+    def music_identifiers(self) -> Optional[str]:
+        return self._metadata.get('identifier')
--- takiyasha/algorithms/ncm/ciphers.py
+++ takiyasha/algorithms/ncm/ciphers.py
@@ -1,37 +1,39 @@
-from ..common import Cipher
-from ...typehints import BytesType
+from Cryptodome.Util.strxor import strxor
+
+from ..common import Cipher, StreamCipher
+from ...typehints import BytesType, BytesType_tuple
 
 
 class NCM_RC4Cipher(Cipher):
     """A cipher that implemented decryption of the NCM encrypted container format.
     
     If the first 8 bytes of the file is equal to bytestring "CTENFDAM", this cipher should be used."""
-    
+
     def __init__(self, key: BytesType):
         if key is None:
             raise TypeError(f"'key' must be byte or bytearray, not None")
         super().__init__(key)
-        
+
         # build S-box
         box: bytearray = bytearray(range(256))
         j: int = 0
         for i in range(256):
             j = (j + box[i] + self.key[i % self.key_length]) & 0xff
             box[i], box[j] = box[j], box[i]
-        
+
         final_box: bytearray = bytearray(256)
         for i in range(256):
             _: int = (i + 1) & 0xff
             si: int = box[_] & 0xff
             sj: int = box[(_ + si) & 0xff] & 0xff
             final_box[i] = box[(si + sj) & 0xff]
-        
+
         self._box: bytearray = final_box
-    
+
     @property
     def box(self):
         return self._box
-    
+
     def decrypt(self, src_data: BytesType, /, offset: int = 0) -> bytes:
         src_data_len: int = len(src_data)
         return bytes(self.box[(offset + i) & 0xff] ^ src_data[i] for i in range(src_data_len))
@@ -42,9 +44,50 @@ class NCM_XOROnlyCipher(Cipher):
     
     If the file format is not recognized and the file does not begin with the byte string "CTENFDAM",
     but you are sure it was downloaded from Cloudmusic, this cipher should be used."""
-    
+
     def __init__(self):
         super().__init__(key=None)
-    
+
     def decrypt(self, src_data: BytesType, /, offset: int = 0) -> bytes:
         return bytes(b ^ 163 for b in src_data)
+
+
+class NCM_NewRC4Cipher(StreamCipher):
+    def __init__(self, key: BytesType):
+        if not isinstance(key, BytesType_tuple):
+            raise TypeError(f"'key' must be bytes or bytearray, not {type(key).__name__}")
+        super().__init__(bytes(key))
+
+        first_box: bytearray = bytearray(range(256))
+        j: int = 0
+        for i in range(256):
+            j = (j + first_box[i] + self.key[i % self.key_length]) & 0xff
+            first_box[i], first_box[j] = first_box[j], first_box[i]
+
+        final_box: bytearray = bytearray(256)
+        for i in range(256):
+            _: int = (i + 1) & 0xff
+            si: int = first_box[_] & 0xff
+            sj: int = first_box[(_ + si) & 0xff] & 0xff
+            final_box[i] = first_box[(si + sj) & 0xff]
+
+        self._box: bytes = bytes(final_box)
+
+    @property
+    def box(self) -> bytes:
+        return self._box
+
+    def decrypt(self, src: bytes, offset: int = 0) -> bytes:
+        stream: bytes = bytes(self._box[(offset + i) & 0xff] for i in range(len(src)))
+
+        return strxor(stream, src)
+
+
+class NCM_NewXORCipher(StreamCipher):
+    def __init__(self):
+        super().__init__(key=None)
+
+    def decrypt(self, src: bytes, offset: int = 0) -> bytes:
+        stream: bytes = bytes([163]) * len(src)
+
+        return strxor(stream, src)
