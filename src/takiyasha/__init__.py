58a4642c4f0479d54af165d592ae6fe9d50b8b31
nukemiko <north666dakota@gmail.com>
2022-05-12 21:09:30 +0800

    删除旧文件

--- src/takiyasha/__init__.py
+++
@@ -1,2 +0,0 @@
-from .algorithms import new_decoder
-from .metadata import new_tag
--- src/takiyasha/__version__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-with open(os.path.join(os.path.dirname(__file__), 'VERSION'), encoding='UTF-8') as f:
-    __version__: str = f.read().strip()
-
-
-def version() -> str:
-    return __version__
