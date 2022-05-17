c3abafb614db478cef599e1883e0c69785bdc961
nukemiko <north666dakota@gmail.com>
2022-05-17 10:23:53 +0800

    添加了旧版解密方案

--- src/takiyasha/qmc/ciphers/keycryption.py
+++ src/takiyasha/qmc/ciphers/keycryption.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
 
 from base64 import b64decode
+from typing import IO
 
+from . import legacyconstants
+from .legacy import Key256Mask128
+from ... import utils
 from ...common import KeylessCipher
 from ...exceptions import DecryptException, InvalidDataError
 from ...standardciphers import TEA
-from ... import utils
 
 
 class ValidationError(DecryptException):
@@ -141,3 +144,89 @@ class QMCv2Key(KeylessCipher):
 
     def encrypt(self, plaindata: bytes, start_offset: int = 0) -> bytes:
         raise NotImplementedError
+
+
+def find_mflac_mask(fileobj: IO[bytes]) -> bytes | None:
+    utils.verify_fileobj_readable(fileobj)
+    utils.verify_fileobj_seekable(fileobj)
+
+    data_len = fileobj.seek(0, 2)
+    test_len = min(0x8000, data_len)
+    fileobj.seek(0, 0)
+    file_header = fileobj.read(4)
+    fileobj.seek(0, 0)
+
+    while fileobj.tell() <= test_len:  # 这里的 tell() 返回 read() 之后的位置
+        data_offset = fileobj.tell()  # 这里的 tell() 返回 read() 之前的位置（mask 的位置）
+        mask = fileobj.read(128)
+        stream = bytes(Key256Mask128.yield_mask(mask, data_offset, 4))
+        if utils.bytesxor(file_header, stream) == b'fLaC':
+            return mask
+
+
+def find_mgg_mask(fileobj: IO[bytes]) -> bytes | None:
+    utils.verify_fileobj_readable(fileobj)
+    utils.verify_fileobj_seekable(fileobj)
+    fileobj.seek(0, 0)
+    data = fileobj.read()
+
+    if len(data) < 0x100:
+        raise ValueError(f"data length should be greater than {0x100}, got {len(data)}")
+
+    mask_confidence: list[dict[int, int]] = [{} for _ in range(44)]
+
+    page2_size = data[0x54] ^ data[0xc] ^ legacyconstants.ogg_public_header1[0xc]
+    spec_header, spec_confidence = generate_ogg_full_header(page2_size)
+    test_len = len(spec_header)
+
+    for idx128 in range(test_len):
+        confidence = spec_confidence[idx128]
+        if confidence > 0:
+            tempmask = data[idx128] ^ spec_header[idx128]
+
+            idx44 = legacyconstants.key256mapping_128to44[idx128 & 0x7f]
+            if mask_confidence[idx44].get(tempmask):
+                mask_confidence[idx44][tempmask] += confidence
+            else:
+                mask_confidence[idx44][tempmask] = confidence
+
+    mask = bytearray(44)
+    for i in range(44):
+        mask[i] = decide_mgg_mask256_item_conf(mask_confidence[i])
+
+    cipher = Key256Mask128(mask)
+    if cipher.decrypt(data[:4], 0).startswith(b'OggS'):
+        return bytes(mask)
+
+
+def generate_ogg_full_header(page_size: int) -> tuple[bytes, list[int]]:
+    spec = bytearray(page_size + 1)
+    spec[0], spec[1], spec[page_size] = page_size & 0xff, 0xff, 0xff
+    for i in range(2, page_size):
+        spec[i] = 0xff
+    spec_confidence: list[int] = [0] * (page_size + 1)
+    spec_confidence[0], spec_confidence[1], spec_confidence[page_size] = 6, 0, 0
+    for i in range(2, page_size):
+        spec_confidence[i] = 4
+
+    all_confidence = legacyconstants.ogg_public_confidence1[:] + spec_confidence + legacyconstants.ogg_public_confidence2[:]
+    all_header = b''.join([legacyconstants.ogg_public_header1, spec, legacyconstants.ogg_public_header2])
+
+    return all_header, all_confidence
+
+
+def decide_mgg_mask256_item_conf(confidence: dict[int, int]) -> int:
+    conf_len = len(confidence)
+    if conf_len == 0:
+        raise ValueError("cannot match at least one key")
+    elif conf_len > 1:
+        # 2 potential value for the mask
+        pass
+    result, conf = 0, 0
+    for idx, item in confidence.items():
+        if item > conf:
+            result = idx
+            result &= 0xff
+            conf = item
+
+    return result
--- src/takiyasha/qmc/ciphers/legacy.py
+++ src/takiyasha/qmc/ciphers/legacy.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import Generator
 
-from ...common import KeylessCipher
+from .legacyconstants import key256mapping_all
+from ...common import Cipher, KeylessCipher
 from ...utils import bytesxor
 
 
@@ -58,4 +59,59 @@ class OldStaticMap(KeylessCipher):
     def decrypt(self, cipherdata: bytes, start_offset: int = 0) -> bytes:
         data_len = len(cipherdata)
         keystream = bytes(self.gen_keystream(start_offset, data_len))
+        return bytesxor(cipherdata, keystream)
+
+
+class Key256Mask128(Cipher):
+    def __init__(self, mask: bytes):
+        if len(mask) == 44:
+            # 从 44 位转换为 128 位
+            key = self.mask44_to_mask128(mask)
+        elif len(mask) == 128:
+            key = mask[:]
+        else:
+            raise ValueError(f'invalid mask length (should be 44 or 128, got {len(mask)}')
+
+        super().__init__(key)
+
+    @staticmethod
+    def mask44_to_mask128(mask44: bytes) -> bytes:
+        if len(mask44) != 44:
+            raise ValueError(f'invalid mask length (should be 44, got {len(mask44)})')
+
+        mask128 = bytearray(128)
+        idx44 = 0
+        for it256 in key256mapping_all:
+            if it256:
+                for idx128 in it256:
+                    mask128[idx128] = mask44[idx44]
+                idx44 += 1
+
+        return bytes(mask128)
+
+    @classmethod
+    def yield_mask(cls,
+                   mask: bytes,
+                   data_offset: int,
+                   data_len: int
+                   ) -> Generator[int, None, None]:
+        index = data_offset - 1
+        mask_idx = (data_offset % 128) - 1
+
+        for _ in range(data_len):
+            index += 1
+            mask_idx += 1
+            if index == 0x8000 or (index > 0x8000 and ((index + 1) % 0x8000 == 0)):
+                index += 1
+                mask_idx += 1
+            if mask_idx >= 128:
+                mask_idx -= 128
+
+            yield mask[mask_idx]
+
+    def gen_mask(self, data_offset: int, data_len: int) -> Generator[int, None, None]:
+        yield from self.yield_mask(self._key, data_offset, data_len)
+
+    def decrypt(self, cipherdata: bytes, start_offset: int = 0) -> bytes:
+        keystream = bytes(self.gen_mask(start_offset, len(cipherdata)))
         return bytesxor(cipherdata, keystream)
