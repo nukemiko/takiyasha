e099184d3e4a839f03cde3068817ec4d14afcc8f
nukemiko <north666dakota@gmail.com>
2022-05-22 12:20:44 +0800

    from .cli import * -> from . import cli；在并行解密之前提示用户可能消耗大量 CPU 资源；添加解密完毕的提示

--- src/takiyasha/__main__.py
+++ src/takiyasha/__main__.py
@@ -1,48 +1,53 @@
 from __future__ import annotations
 
 import multiprocessing as mp
+import sys
 from time import sleep
 
-from .cli import *
+from . import cli
 
 if __name__ == '__main__':
     mp.set_start_method('spawn')
-    openfile_kwargs = vars(ap.parse_args())
+    openfile_kwargs = vars(cli.ap.parse_args())
 
-    srcpaths: list[Path] = openfile_kwargs.pop('srcpaths')
-    destdir: Path = openfile_kwargs.pop('destdir')
+    srcpaths: list[cli.Path] = openfile_kwargs.pop('srcpaths')
+    destdir: cli.Path = openfile_kwargs.pop('destdir')
     destdir_is_srcdir: bool = openfile_kwargs.pop('destdir_is_srcdir')
     recursive: bool = openfile_kwargs.pop('recursive')
     show_details: bool = openfile_kwargs.pop('show_details')
     enable_multiprocessing: bool = openfile_kwargs.pop('enable_multiprocessing')
     try_fallback: bool = openfile_kwargs.pop('try_fallback')
 
-    tasked_paths = gen_srcs_dsts(*srcpaths,
-                                 destdir=destdir,
-                                 destdir_is_srcdir=destdir_is_srcdir,
-                                 recursive=recursive
-                                 )
+    tasked_paths = cli.gen_srcs_dsts(*srcpaths,
+                                     destdir=destdir,
+                                     destdir_is_srcdir=destdir_is_srcdir,
+                                     recursive=recursive
+                                     )
 
     if len(tasked_paths) == 0:
-        print_stdout('错误：输入路径中没有任何受支持的文件')
-        print_stdout("提示：如果输入路径中存在目录，您应当加上 '-r' 或 '--recursive' 选项")
+        cli.print_stdout('错误：输入路径中没有任何受支持的文件')
+        cli.print_stdout("提示：如果输入路径中存在目录，您应当加上 '-r' 或 '--recursive' 选项")
         sys.exit(1)
 
     if not openfile_kwargs['probe_content']:
-        print_stdout("警告：您添加了 '--faster' 选项")
-        print_stdout('将会跳过文件内容，仅根据文件名判断加密类型')
-        print_stdout('可能会导致部分文件无法识别和解密')
+        cli.print_stdout("警告：您添加了 '--faster' 选项")
+        cli.print_stdout('将会跳过文件内容，仅根据文件名判断加密类型')
+        cli.print_stdout('可能会导致部分文件无法识别和解密')
 
     fallback_params = {
         'legacy_fallback': True
     }
     if try_fallback:
-        print_stdout("提示：您添加了 '-f' 或 '--try-fallback' 选项")
-        print_stdout('将在单个文件首次解密失败时，使用旧方案再次尝试')
-        print_stdout('（仅限部分支持的加密类型，且不保证成功）')
+        cli.print_stdout("提示：您添加了 '-f' 或 '--try-fallback' 选项")
+        cli.print_stdout('将在单个文件首次解密失败时，使用旧方案再次尝试')
+        cli.print_stdout('（仅限部分支持的加密类型，且不保证成功）')
         openfile_kwargs.update(fallback_params)
 
     if enable_multiprocessing:
+        cli.print_stdout("提示：您添加了 '-p' 或 '--parallel' 选项")
+        cli.print_stdout('将会使用多进程并行解密所有输入文件')
+        cli.print_stdout('可能会消耗大量 CPU 资源！')
+
         task_procs: list[mp.Process] = []
         for cursrcfile, curdstdir in tasked_paths:
             params = {
@@ -52,7 +57,7 @@ if __name__ == '__main__':
             }
             params.update(openfile_kwargs)
 
-            p = mp.Process(target=task, kwargs=params, name=str(cursrcfile))
+            p = mp.Process(target=cli.task, kwargs=params, name=str(cursrcfile))
             p.start()
             task_procs.append(p)
 
@@ -65,11 +70,12 @@ if __name__ == '__main__':
                         break
         except KeyboardInterrupt:
             print()
-            print_stderr('过程被用户终止')
+            cli.print_stderr('过程被用户终止')
             for p in task_procs:
                 p.kill()
             sys.exit(130)
         else:
+            cli.print_stdout('所有文件均已解密完毕')
             sys.exit()
     else:
         try:
@@ -81,10 +87,11 @@ if __name__ == '__main__':
                 }
                 params.update(openfile_kwargs)
 
-                task(**params)
+                cli.task(**params)
         except KeyboardInterrupt:
             print()
-            print_stderr('过程被用户终止')
+            cli.print_stderr('过程被用户终止')
             sys.exit(130)
         else:
+            cli.print_stdout('所有文件均已解密完毕')
             sys.exit()
