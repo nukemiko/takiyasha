cdc9e903d429e3471d0798522e92167a2a21003b
nukemiko <north666dakota@gmail.com>
2022-05-31 13:35:49 +0800

    添加了对 QMCv2 文件的保存能力支持

--- src/takiyasha/qmc/__init__.py
+++ src/takiyasha/qmc/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from io import BytesIO
 from typing import IO, Literal
 
-from .ciphers.keycryption import decrypt_QMCv2_key, find_mflac_mask, find_mgg_mask
+from .ciphers.keycryption import decrypt_QMCv2_key, encrypt_QMCv2_key, find_mflac_mask, find_mgg_mask
 from .ciphers.legacy import Key256Mask128, OldStaticMap
 from .ciphers.modern import DynamicMap, ModifiedRC4, StaticMap
 from .. import utils
@@ -20,10 +20,12 @@ class QMCv1(Crypter):
 
     >>> qmcv1file = QMCv1('./test.qmcflac')
     >>> data = qmcv1file.read()
+    >>>
 
     写入：
 
     >>> qmcv1file.write(b'Writted bytes')
+    >>>
 
     创建、写入并保存：
 
@@ -31,7 +33,8 @@ class QMCv1(Crypter):
     >>> with open('./source.flac', 'rb') as f:  # 写入未加密的文件数据
     ...     new_qmcv1file.write(f.read())
     >>> new_qmcv1file.save('./result.qmcflac')
-    >>> """
+    >>>
+    """
 
     @staticmethod
     def file_headers() -> dict[bytes, str]:
@@ -136,6 +139,7 @@ class QMCv2(Crypter):
 
     >>> qmcv2file = QMCv2('./test.mflac')
     >>> data = qmcv2file.read()
+    >>>
     
     读取没有可用密钥的 QMCv2 格式文件：
     
@@ -153,10 +157,15 @@ class QMCv2(Crypter):
     创建空文件并写入：
 
     >>> empty_qmcv2file = QMCv2()
-    >>> empty_qmcv2file.write(b'Writted bytes')
+    >>> with open('source.flac', 'rb') as f:
+    ...     empty_qmcv2file.write(f.read())
     >>>
 
-    目前不支持保存为 QMCv2 文件。
+    保存为 QMCv2 文件：
+
+    >>> empty_qmcv2file.save('target.mflac')
+    >>> empty_qmcv2file.save('target.mflac0', use_qtag=True, songid=114514810)
+    >>>
     """
 
     @staticmethod
@@ -170,9 +179,7 @@ class QMCv2(Crypter):
                  filething: utils.FileThing | None = None,
                  **kwargs
                  ) -> None:
-        """读取 QQ 音乐 QMCv2 格式的文件。
-
-        目前支持向 QMCv2 对象写入数据，但不支持保存为 QMCv2 格式的文件。
+        """读写 QQ 音乐 QMCv2 格式的文件。
 
         部分 QMCv2 格式文件因为没有可用的密钥，目前无法读写，
         可从它们末尾四个字节的十六进制值判断：
@@ -180,18 +187,18 @@ class QMCv2(Crypter):
         - ``25 02 00 00``：来自版本 18.57 及以上的 QQ 音乐 PC 客户端，密钥使用了新的加密方案
         - ``53 54 61 67`` （``STag``）：来自版本 11.5.5 及以上的 QQ 音乐 Android 客户端，没有内置密钥
 
-        可以尝试使用后备方案（指定 ``try_fallback=True``）读取无可用密钥的文件，
+        可以尝试使用后备方案（指定 ``try_fallback=True``）读取这类无可用密钥的文件，
         但后备方案针对这些文件并不一定总是有效。
 
         Args:
             filething (file): 指向源文件的路径或文件对象；留空则视为创建一个空的 QMCv2 文件
         Keyword Args:
-            key (bytes): 加/解密数据所需的密钥；留空则会随机创建一个；
+            key (bytes): 加/解密数据所需的密钥；留空则会随机生成一个；
                 仅在 ``filething`` 为空时有效
             cipher_type (str): 加密类型，仅在 ``filething`` 为空时有效；
-                支持：``dynamic_map``、``rc4``；
+                支持：``dynamic_map``（默认值）、``rc4``；
             try_fallback (bool): 如果无法找到可用的密钥，是否尝试使用后备方案，
-                默认为 False
+                默认为 False；仅在从 QMCv2 文件读取时有效
         Raises:
             ValueError: 为参数 ``cipher_type`` 指定了不支持的值
 
@@ -249,16 +256,20 @@ class QMCv2(Crypter):
              ) -> None:
         """将一个 QMCv2 文件加载到当前 QMCv2 对象中。
 
-        在此过程中需要先找到文件中可用的密钥；如果没有可用的密钥，
-        可加上参数 ``try_fallback=True`` 使用后备方案再次尝试。
+        在此过程中需要先找到文件中可用的密钥：
+
+        如果找到了可用的密钥，那么给 ``__init__()`` 传递的 ``key``
+        和 ``cipher_type`` 参数将会被探测到的密钥和加密算法类型取代。
+
+        如果没有可用的密钥，可加上参数 ``try_fallback=True``使用后备方案再次尝试。
 
         后备方案针对无可用密钥的文件并不一定总是有效。
 
         Args:
             filething (file): 源 QMCv2 文件的路径或文件对象
         Keyword Args:
-            try_fallback (bool): 如果无法找到可用的密钥，
-                是否尝试使用后备方案，默认为 False
+            try_fallback (bool): 如果无法找到可用的密钥，是否尝试使用后备方案，
+            默认为 False
         Raises:
             FileTypeMismatchError: ``filething`` 不是一个 QMCv2 格式文件
             UnsupportedFileType: ``filething`` 是一个 QMCv2 文件，但其格式不受支持
@@ -284,10 +295,10 @@ class QMCv2(Crypter):
                     audio_len = fileobj.seek(-(4 + int.from_bytes(tail, 'little')), 2)
                 else:
                     audio_len = fileobj.seek(-4, 2)
-                raw_keydata: bytes | None = find_mflac_mask(fileobj) or find_mgg_mask(fileobj)
+                b64encoded_ciphered_keydata: bytes | None = find_mflac_mask(fileobj) or find_mgg_mask(fileobj)
                 songid = None
                 unknown = None
-                if not raw_keydata:
+                if not b64encoded_ciphered_keydata:
                     raise UnsupportedFileType(f'all attempt of decrypt failed: '
                                               f'{self.unsupported_file_tailer()[tail]}'
                                               )
@@ -296,22 +307,22 @@ class QMCv2(Crypter):
                                           f'{self.unsupported_file_tailer()[tail]}'
                                           )
         elif tail == b'QTag':
-            audio_len, raw_keydata, songid, unknown = self.get_qtag(fileobj)
+            audio_len, b64encoded_ciphered_keydata, songid, unknown = self.get_qtag(fileobj)
         else:
             fileobj.seek(-4, 2)
             raw_key_len = int.from_bytes(fileobj.read(4), 'little')
             if 0 < raw_key_len <= 0x300:
                 audio_len = fileobj.seek(-(4 + raw_key_len), 2)
-                raw_keydata = fileobj.read(raw_key_len)
+                b64encoded_ciphered_keydata = fileobj.read(raw_key_len)
                 songid = None
                 unknown = None
             else:
                 raise FileTypeMismatchError('not a QMCv2 file: unknown file tail or key not found')
 
         if tail in self.unsupported_file_tailer() and try_fallback:
-            self._cipher: DynamicMap | ModifiedRC4 | Key256Mask128 = Key256Mask128(raw_keydata)
+            self._cipher: DynamicMap | ModifiedRC4 | Key256Mask128 = Key256Mask128(b64encoded_ciphered_keydata)
         else:
-            key = decrypt_QMCv2_key(raw_keydata)
+            key = decrypt_QMCv2_key(b64encoded_ciphered_keydata)
             if 0 < len(key) < 300:
                 self._cipher: DynamicMap | ModifiedRC4 | Key256Mask128 = DynamicMap(key)
             else:
@@ -329,15 +340,53 @@ class QMCv2(Crypter):
              ) -> None:
         """将当前 QMCv2 对象保存为一个 QMCv2 格式文件。
 
-        警告：目前不支持保存到文件；尝试调用本方法会引发 ``NotImplementedError``
-
         Args:
             filething (file): 目标 QMCv2 文件的路径或文件对象；
                 留空则尝试使用 ``self.name``；如果两者都为空，抛出 ``ValueError``
+        Keyword Args:
+            use_qtag (bool): 是否在输出的 QMCv2 文件中写入 QTag，默认为 False
+            songid (int): QTag 中的歌曲 ID，可以留空；仅在 ``use_qtag=True`` 时有效
         Raises:
             ValueError: 同时缺少参数 ``filething`` 和属性 ``self.name``
-            NotImplementedError: 在调用本方法时引发；此异常将在未来加入功能后移除"""
-        raise NotImplementedError
+
+        所有未知的关键字参数都会被忽略。
+        """
+        use_qtag: bool = kwargs.get('use_qtag', False)
+
+        encoded_encrypted_keydata = encrypt_QMCv2_key(self._cipher.key)
+
+        if use_qtag:
+            songid: int | None = kwargs.get('songid', self._songid)
+            if songid is None:
+                songid = 0
+            qtag_unknown_value = 2 if self._qtag_unknown is None else self._qtag_unknown
+            qtag = b','.join([
+                encoded_encrypted_keydata,
+                str(songid).encode(),
+                str(qtag_unknown_value).encode()]
+            )
+            qtag_size = len(qtag).to_bytes(4, 'big')
+            additionals = qtag + qtag_size + b'QTag'
+        else:
+            encoded_encrypted_keydata_size = len(encoded_encrypted_keydata).to_bytes(4, 'little')
+            additionals = encoded_encrypted_keydata + encoded_encrypted_keydata_size
+
+        if filething:
+            if utils.is_filepath(filething):
+                fileobj: IO[bytes] = open(filething, 'wb')  # type: ignore
+            else:
+                fileobj: IO[bytes] = filething  # type: ignore
+                utils.verify_fileobj_writable(fileobj, bytes)
+        elif self._name:
+            fileobj: IO[bytes] = open(self._name, 'wb')
+        else:
+            raise ValueError('missing filepath or fileobj')
+
+        self._raw.seek(0, 0)
+        fileobj.write(self._raw.read())
+        fileobj.write(additionals)
+        if utils.is_filepath(filething):
+            fileobj.close()
 
     @property
     def cipher(self) -> DynamicMap | ModifiedRC4 | Key256Mask128:
