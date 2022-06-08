a64a6f70025de9a6e3a9861d60abe7df0b9af128
nukemiko <north666dakota@gmail.com>
2022-06-09 00:02:57 +0800

    修复了命令行帮助信息中的程序名错误

--- src/takiyasha/constants.py
+++ src/takiyasha/constants.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-PROGNAME = Path(__file__).parent.parent.name
+PROGNAME = Path(__file__).parent.name
 DESCRIPTION = f'  {PROGNAME} - Python 版本的音乐解密工具'
 EPILOG = f'{PROGNAME} 对输出数据的可用性（是否可以识别、播放等）不做任何保证。\n\n' \
          f'{PROGNAME} 的默认行为（为输出文件补全标签）需要联网，\n' \
