4d1cacf92a8dc543300fbc9c913053dd5fca4fe0
nukemiko <north666dakota@gmail.com>
2022-07-12 20:35:46 +0800

    删除了已经拆分出去的 `libtakiyasha` 包

--- src/libtakiyasha/qmc/ciphers/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from .legacy import Key256Mask128, OldStaticMap
-from .modern import DynamicMap, ModifiedRC4, StaticMap
-
-__all__ = ['DynamicMap', 'Key256Mask128', 'ModifiedRC4', 'OldStaticMap', 'StaticMap']
